1c054b7909a1da88bdd85306056d63ac3cf8f72c
kazizi55 <kaz.redhot1201@gmail.com>
2025-08-12 10:08:44 +0900

    Add comments to step4

--- arai60/141-linked-list-cycle/step4.py
+++ arai60/141-linked-list-cycle/step4.py
@@ -1,15 +1,15 @@
 class RevisedSolution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        reached_nodes = set()
+        visited_nodes = set()
         node = head
 
         # MEMO: None などの singleton との比較には is not を使うようにする
         # MEMO: 条件文に括弧をつける・つけないは統一する
         while node is not None:
-            if node in reached_nodes:
+            if node in visited_nodes:
                 return True
             else:
-                reached_nodes.add(node)
+                visited_nodes.add(node)
                 node = node.next
         
         return False

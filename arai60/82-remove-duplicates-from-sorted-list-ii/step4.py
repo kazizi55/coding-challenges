46631e03651c94513d2f352003828c4c88ac923f
kazizi55 <kaz.redhot1201@gmail.com>
2025-09-18 23:23:39 +0900

    step4

--- arai60/82-remove-duplicates-from-sorted-list-ii/step4.py
+++ arai60/82-remove-duplicates-from-sorted-list-ii/step4.py
@@ -1,6 +1,7 @@
 class SolutionWithSeparationAndHandOver:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(next=head)
+        dummy = ListNode()
+        dummy.next = head
         previous = dummy
         current = head
         val_to_remove = None
@@ -22,7 +23,8 @@ class SolutionWithSeparationAndHandOver:
     
 class SolutionWithoutValueToSkip:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(next=head)
+        dummy = ListNode()
+        dummy.next = head
         previous = dummy
         current = head
 
@@ -40,7 +42,7 @@ class SolutionWithoutValueToSkip:
 	
 class SolutionWithMoveToDistinctNode:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def moveToDistinctNode(node: Optional[ListNode], val_to_skip: int) -> ListNode:
+        def moveToNextDistinctNode(node: Optional[ListNode], val_to_skip: int) -> ListNode:
             while node is not None and node.val == val_to_skip:
                 node = node.next
             return node
@@ -51,10 +53,31 @@ class SolutionWithMoveToDistinctNode:
 
         while current is not None:
             if current.next is not None and current.val == current.next.val:
-                current = moveToDistinctNode(current.next, current.val)
+                current = moveToNextDistinctNode(current.next, current.val)
                 previous.next = None
                 continue
             previous.next = current
             previous = previous.next
             current = current.next
+        return dummy.next
+
+class SolutionWithClearVariableNames:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def moveToDistinctNode(node: Optional[ListNode], val_to_skip: int) -> ListNode:
+            while node is not None and node.val == val_to_skip:
+                node = node.next
+            return node
+        dummy = ListNode()
+        dummy.next = head
+        tail = dummy
+        node = head
+
+        while node is not None:
+            if node.next is not None and node.val == node.next.val:
+                node = moveToDistinctNode(node.next, node.val)
+                tail.next = None
+                continue
+            tail.next = node
+            tail = tail.next
+            node = node.next
         return dummy.next
